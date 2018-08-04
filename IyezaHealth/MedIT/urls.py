0c1cc8f02aef186d05e23f32de13cbcac6fd7dc5
thandile_xiphu <thandile.xiphu@argonasset.co.za>
2018-08-04 17:06:21 +0200

    CFG: adding app urls

--- IyezaHealth/MedIT/urls.py
+++ IyezaHealth/MedIT/urls.py
@@ -4,6 +4,17 @@ from MedIT import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<patient_id>\d+)/$', views.detail, name='patient_detail'),
+    url(r'^login', views.login, name='login'),
+    url(r'^signup', views.sign_up, name='sign_up'),
+    url(r'^(?P<patient_id>\d+)/$', views.view_patient_details,
+        name='view_patient_details'),
+    url(r'^editPatientDetails', views.edit_patient_details,
+        name='edit_patient_details'),
+    url(r'^editPrescriptionDetails', views.edit_prescription_details,
+        name='edit_prescription_details'),
+    url(r'^viewPrescriptionDetails', views.view_prescription_details,
+        name='view_prescription_details'),
+    url(r'^viewDeliverySchedule', views.view_delivery_schedule,
+        name='view_delivery_schedule')
 
 )
