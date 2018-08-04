4824afd9ed269e055fed63da58b58fd1b48008c7
thandile_xiphu <thandile.xiphu@argonasset.co.za>
2018-08-04 15:05:50 +0200

    CFG: configures project settings

--- IyezaHealth/IyezaHealth/settings.py
+++ IyezaHealth/IyezaHealth/settings.py
@@ -1,5 +1,5 @@
 # Django settings for IyezaHealth project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'medit',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -69,9 +69,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+   #os.path.join('IyezaHealth', 'static')
 )
 
 # List of finder classes that know how to find static files in
@@ -108,9 +106,7 @@ ROOT_URLCONF = 'IyezaHealth.urls'
 WSGI_APPLICATION = 'IyezaHealth.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    #os.path.join('IyezaHealth', 'templates')
 )
 
 INSTALLED_APPS = (
@@ -120,10 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'MedIT',
 )
 
 # A sample logging configuration. The only tangible logging
--- IyezaHealth/MedIT/models.py
+++ IyezaHealth/MedIT/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    id_no = models.CharField(max_length=13)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=500)
+    email = models.EmailField(max_length=500)
+    cellphone = models.BigIntegerField()
+    gender = models.CharField(max_length=10)
+    next_of_kin_name = models.CharField(max_length=50)
+    next_of_kin_cell = models.CharField(max_length=10)
+
+
+class MedicalInfo(models.Model):
+    id_no = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor_name = models.CharField(max_length=50)
+    doctor_cell = models.BigIntegerField()
+    medical_aid_no = models.BigIntegerField()
+    medical_aid_scheme = models.CharField(max_length=50)
+
+
+class Prescriptions(models.Model):
+    id_no = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    treatment_type = models.CharField(max_length=80)
+    clinic_pickup = models.CharField(max_length=100)
+    medications = models.CharField(max_length=200)
+    pick_date = models.DateField()
+    treatment_status = models.CharField(max_length=20, choices=(('Current', 'Current'),
+                                        ('Discontinued', 'Discontinued')))
